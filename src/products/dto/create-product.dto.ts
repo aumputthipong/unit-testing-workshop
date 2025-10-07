70a1149999f9d1427e184f1dd45daa7158041802
aumputthipong <aumputthipong2002@gmail.com>
2025-10-07 19:46:27 +0700

    edit:dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -2,4 +2,7 @@ export class CreateProductDto {
   productName: string;
   productPrice: number;
   productStock: number;
+  productDetail: string;
+  productImage: string;
+  Availability: boolean;
 }
